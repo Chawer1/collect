06d5845ff67b18e8c4790c4c2f01e1ca2cd654a9
Chawert <48294381+Chawert@users.noreply.github.com>
2020-04-01 20:59:55 +0300

    'heroku'

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Collect
-  class Application < Rails::Application
+  class Application < ActiveSupport::LoggerSilence
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.time_zone = 'Minsk'
