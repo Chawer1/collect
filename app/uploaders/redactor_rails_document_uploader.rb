6444ef7c1313b2f1f63bc872274e1bd9bf2d0404
Chawert <48294381+Chawert@users.noreply.github.com>
2020-04-01 22:52:34 +0300

    'pomogi'

--- app/uploaders/redactor_rails_document_uploader.rb
+++ app/uploaders/redactor_rails_document_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class RedactorRailsDocumentUploader < CarrierWave::Uploader::Base
-  include RedactorRails::Backend::CarrierWave
+  include RedactorRails::CarrierWave
 
   # storage :fog
   storage :file
